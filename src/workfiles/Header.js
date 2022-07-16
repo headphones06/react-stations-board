aa4e066bc3c4950632acb949de64177ef6a4d46e
headphones06 <headphones.CG@gmail.com>
2022-07-16 16:02:45 +0900

    スレッド一覧の作成

---
+++ src/workfiles/Header.js
@@ -0,0 +1,11 @@
+import 'App.css';
+
+export function Header(){
+  return(
+    <header className="App-header">
+      <p>
+        掲示板
+      </p>
+    </header>
+  )
+}
---
+++ src/workfiles/ThreadList.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useState} from 'react';
+import 'workfiles/ThreadList.css';
+
+export function ThreadList(){
+  const [selectThread, setSelectThread] = useState("");
+  const [threadList, setThreadList] = useState([]);
+  const [count, setCount] = useState(0);
+  const increment = () => setCount((prevValue) => prevValue + 10);
+  const decrement = () => setCount((prevValue) => prevValue - 10);
+
+  useEffect(() => {
+    if(count < 0){
+      setCount(0);
+    }else{
+      fetch("https://railway-react-bulletin-board.herokuapp.com/threads?offset=" + count)
+      .then(res => res.json())
+      .then(data => {
+        setThreadList(data)
+        console.log(data)
+      })
+    }
+  }, [count])
+
+  const listUp = threadList.map((thre) => {
+    return(
+      <div className="container">
+        <h2 className = "title">{thre.title}</h2>
+        <p className = "id">{thre.id}</p>
+      </div>
+    )
+  })
+
+  return(
+    <div>
+      <h1>スレッド一覧</h1>
+      <div>
+        {listUp}
+      </div>
+      <div>
+        <button type="button" onClick={decrement}>前の10件</button>
+        <button type="button" onClick={increment}>次の10件</button>
+      </div>
+    </div>
+  )
+}
+
